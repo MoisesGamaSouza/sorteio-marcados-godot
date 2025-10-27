bd90aa165eda0f0046447641d3134b3e77849b09
MoisesGamaSouza <118690940+MoisesGamaSouza@users.noreply.github.com>
2025-10-27 07:48:21 -0300

    25/10/2025
    - Método de criar um modelo JSON da planilha principal na maquina do usuário, para que ele possa seguir um padrão.

--- Scripts/control.gd
+++ Scripts/control.gd
@@ -1,37 +1,11 @@
 extends Control
 
-var mywin = preload("res://Scenes/DisplayWindow.tscn")
+#var mywin = preload("res://Scenes/DisplayWindow.tscn")
 
 func _ready():
 	pass
 
-func ativarSegundaTela(i): #Função que recebe o ID do monitor escolhido e Cria a segunda tela
-	FecharTela()#teste para conferir se já não há uma segunda tela criada, e se houver, fechando ela antes de abrir outra
-	
-	var d = mywin.instantiate() 
-	add_child(d) #Adicionando o Nó da tela como filho e logo em seguida Configurando seus atributos
-	d.visible = true
-	d.position = DisplayServer.screen_get_position(i) #estará posicionado no Monitor Escolhido
-	d.size = DisplayServer.screen_get_size(i) #Tera o tamanho maximo do monitor escolhido
-	d.always_on_top = true #Estará sempre no topo
-	#d.borderless = true #Não terá borda
-	
-	Global.wininstance = d #Guardando a inscancia da janela em uma variavel para maior controle
-
-func FecharTela():
-	if Global.wininstance != null:
-		Global.wininstance.queue_free()
-		#Global.wininstance = null
-		
 #Garantindo que o programa poderá ser fechado caso a janela surja onde não deve
 func _input(event):
 	if event.is_action_pressed("Fechar"):
 		get_tree().quit()
-
-
-func _on_button_apply_pressed() -> void:
-	ativarSegundaTela(get_node("Controle de Monitor/OptionButton").get_selected_id())
-
-
-func _on_button_fechar_pressed() -> void:
-	FecharTela()
--- Scripts/controle_de_monitor.gd
+++ Scripts/controle_de_monitor.gd
@@ -1,26 +1,56 @@
 extends Control
 
-@onready var om = $OptionButton #Opções de Monitores
+@onready var om = %OptionButton #Opções de Monitores
+@onready var st = %"Segunda Tela"
 var testwin = preload("res://Scenes/DisplayTest.tscn")
+var mywin = preload("res://Scenes/DisplayWindow.tscn")
 
 func _ready() -> void:
-	om.clear()	#Limpando a Lista de Seleção
+	atualizarMonitores()
+		
+func ativarSegundaTela(i): #Função que recebe o ID do monitor escolhido e Cria a segunda tela
+	FecharTela()#teste para conferir se já não há uma segunda tela criada, e se houver, fechando ela antes de abrir outra
 	
+	var d = mywin.instantiate() 
+	st.add_child(d) #Adicionando o Nó da tela como filho e logo em seguida Configurando seus atributos
+	d.visible = true
+	d.position = DisplayServer.screen_get_position(i) #estará posicionado no Monitor Escolhido
+	d.size = DisplayServer.screen_get_size(i) #Tera o tamanho maximo do monitor escolhido
+	d.always_on_top = true #Estará sempre no topo
+	#d.borderless = true #Não terá borda
+	
+	Global.wininstance = d #Guardando a inscancia da janela em uma variavel para maior controle
+
+func FecharTela():
+	if Global.wininstance != null:
+		Global.wininstance.queue_free()
+		#Global.wininstance = null
+
+func atualizarMonitores() -> void:
+	om.clear()	#Limpando a Lista de Seleção
 	# Adicionando os monitores presentes no computador dentro da lista de seleção
 	for i in range(DisplayServer.get_screen_count()):
 		om.add_item(str(i))
-		
+
 # Função que cria uma janela como uma especie de PopUp para mostrar qual monitor está sendo selecionado antes de de fato criar a janela principal nele.
-func testeMonitor(i):
+func testeMonitor(index: int) -> void:
 	var d = testwin.instantiate()
-	add_child(d)
+	st.add_child(d)
 	d.visible = true
-	d.position = DisplayServer.screen_get_position(i)
+	d.position = DisplayServer.screen_get_position(index)
 	d.title = "Monitor Selecionado"
 	d.size = Vector2(100, 100)
 	d.always_on_top = true
-	d.get_node("Label").text = str($OptionButton.get_selected_id())
-	
+	d.get_node("Label").text = str(om.get_selected_id())
+
+
+
 func _on_button_test_pressed() -> void:
-	testeMonitor($OptionButton.get_selected_id())
+	testeMonitor(om.get_selected_id())
 	pass # Replace with function body.
+
+func _on_button_apply_pressed() -> void:
+	ativarSegundaTela(om.get_selected_id())
+
+func _on_button_fechar_pressed() -> void:
+	FecharTela()
